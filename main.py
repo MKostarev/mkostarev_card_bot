280d196019d6e669656666acf1decd1340e8e4da
Mikhail Kostarev <117512786+MKostarev@users.noreply.github.com>
2023-02-24 12:19:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,14 +4,54 @@ import config
 
 bot = telebot.TeleBot(config.TOKEN)
 
-@bot.message_handler(commands= ['start'])
+@bot.message_handler(commands= ['start']) #/start - то вызов приветственного экрана который ты сейчас читаешь
+def start_main_menu(message):
+    bot.send_message(message.chat.id, """Привет! 
+Это бот помощник и он же моё портфолио. 
+
+Давай я тебе расскажу что он может.
+
+"Меню":
+/start - то вызов приветственного экрана который ты сейчас читаешь
+/help - подсказка с командами
+/about_me - тут я расскажу о себе, своих достижениях, своих целях
+/weather - узнаём погоду
+/currency_rate - курс валют
+/to_do_list - напоминания
+/calculator - простой калькулятор
+/.........
+""")
+
+
+@bot.message_handler(commands= ['about_me']) #/start - то вызов приветственного экрана который ты сейчас читаешь
+def about_me_main_menu(message):
+    about_me_board = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    biography = types.KeyboardButton('Биография')
+    portfolio = types.KeyboardButton('Портфолио')
+    contacts = types.KeyboardButton('Контакты')
+    about_me_board.add(biography, portfolio, contacts)
+    bot.send_message(message.chat.id, 'Добропожаловать в бот визитку', reply_markup=about_me_board)
+
+@bot.message_handler(commands= ['help']) #/help - подсказка с командами
 def main_menu(message):
-    startBoard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    menu = types.KeyboardButton('Услуги')
-    startBoard.add(menu)
-    bot.send_message(message.chat.id, 'Добропожаловать в бот визитку', reply_markup=startBoard)
+    #startBoard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    #menu = types.KeyboardButton('Услуги')
+    #startBoard.add(menu)
+    bot.send_message(message.chat.id, 'Добропожаловать в бот визитку, задел Help')
 
 
+
+#/help - подсказка с командами
+#
+#/about_me - тут я расскажу о себе, своих достижениях, своих целях
+#
+#/weather - узнаём погоду
+#
+#/currency_rate - курс валют
+#
+#/to_do_list - напоминания
+#
+#/calculator - простой калькулятор
 
 #@bot.message_handler(commands=['start'])
 #def main_menu(message):
